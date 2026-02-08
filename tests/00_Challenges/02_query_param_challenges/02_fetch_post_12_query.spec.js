ca6627adee7b42906554b135d9fc7abc64293abd
Shivakrishna Dharmavaram <shivakrishnadss1999@gmail.com>
2026-02-08 19:16:49 +0530

    query parameter handsOn

---
+++ tests/00_Challenges/02_query_param_challenges/02_fetch_post_12_query.spec.js
@@ -0,0 +1,16 @@
+//Fetch a post with id 102 and validate status code using query parameter
+import {test, expect} from '@playwright/test';
+
+test('Fetch a post with id 12 and validate status code using query parameter', async ({request}) => {
+    // const url = 'https://jsonplaceholder.typicode.com/posts?id=12';
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const queryParams = { id: 12 }; // Define query parameters
+    const response = await request.get(url, { params: queryParams }); // Send a GET request to the specified URL with query parameter id=12 and returns a promise of responseAPI.
+    // Validate the response status code
+    expect(response.status()).toBe(200);
+
+    console.log('Response :', await response.json());
+
+    console.log('Response Status Code:', response.status());
+    console.log('Type of Response:', typeof response);
+});
